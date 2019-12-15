a1da990d1a9f35219335512fd575c01d40edc799
Yura Slinkin <jurij.jurich@gmail.com>
2019-12-15 12:40:53 +0100

    block and single line expressions

--- src/main/scala/plugin/ContextApplied.scala
+++ src/main/scala/plugin/ContextApplied.scala
@@ -7,6 +7,7 @@ import nsc.plugins.PluginComponent
 import nsc.transform.Transform
 import nsc.transform.TypingTransformers
 import nsc.ast.TreeDSL
+import scala.collection.mutable.ListBuffer
 import scala.reflect.internal.Flags._
 
 class ContextApplied(val global: Global) extends Plugin {
@@ -33,21 +34,115 @@ class ContextPlugin(plugin: Plugin, val global: Global)
   class MyTransformer(unit: CompilationUnit) extends TypingTransformer(unit) {
     val global: ContextPlugin.this.global.type = ContextPlugin.this.global
 
-    def valDef(name: String): Tree =
-      ValDef(Modifiers(SYNTHETIC | ARTIFACT), TermName(name), Ident(TypeName("Int")), Literal(Constant(12)))
+    def nullVal(name: String, typeName: TypeName): Tree =
+      ValDef(Modifiers(), TermName(name), Ident(typeName), Literal(Constant(null)))
+
+    def defineTrait(name: String, parent: Option[String], inside: DefDef): Tree =
+      ClassDef(
+        Modifiers(ABSTRACT | DEFAULTPARAM / TRAIT),
+        TypeName(name),
+        List(),
+        Template(List(Ident(TypeName(parent.getOrElse("AnyRef")))), noSelfType, List(traitInit, inside))
+      )
+
+    def defineEmptyTrait(tpName: TypeName): Tree =
+      ClassDef(
+        Modifiers(ABSTRACT | INTERFACE | DEFAULTPARAM / TRAIT),
+        tpName,
+        List(),
+        Template(List(Ident(TypeName("AnyRef"))), noSelfType, List())
+      )
+
+    def traitInit: DefDef =
+      DefDef(Modifiers(), termNames.MIXIN_CONSTRUCTOR, List(), List(List()), TypeTree(), Block(List(), Literal(Constant(()))))
+
+    def defineObject(name: String, parent: Option[String], inside: DefDef): ModuleDef =
+      ModuleDef(
+        Modifiers(),
+        TermName(name),
+        Template(
+          List(Ident(TypeName(parent.getOrElse("AnyRef")))),
+          noSelfType,
+          List(moduleInit(parent.isDefined), inside)
+        )
+      )
+
+    def moduleInit(parent: Boolean): DefDef =
+      DefDef(
+        Modifiers(),
+        termNames.CONSTRUCTOR,
+        List(),
+        List(List()),
+        TypeTree(),
+        Block(
+          List(
+            Apply(
+              Select(
+                Super(
+                  This(typeNames.EMPTY),
+                  typeNames.EMPTY
+                ),
+                if (parent) termNames.MIXIN_CONSTRUCTOR
+                else termNames.CONSTRUCTOR
+              ),
+              List()
+            )
+          ),
+          Literal(Constant(()))
+        )
+      )
+
+    def importModule(name: String): Tree =
+      Import(Ident(TermName(name)), List(ImportSelector.wild))
+
+    def defineImplicitConv(fromT: TypeName, resT: TypTree, resV: String): DefDef =
+      DefDef(
+        Modifiers(IMPLICIT),
+        TermName(s"${fromT}_$resT"),
+        List(),
+        List(List(ValDef(Modifiers(PARAM), TermName("e"), Ident(fromT), EmptyTree))),
+        resT,
+        Ident(TermName(resV))
+      )
+
+    def createTraits(bound: ContextBound): List[Tree] = {
+      val trees = new ListBuffer[Tree]
+
+      val empty = TypeName("E$")
+      trees.append(defineEmptyTrait(empty))
+
+      val lastParent = bound.evs.tail.foldRight(Option.empty[String]) { case (ev, parent) =>
+        val name = s"${ev.name.tpt}Conv"
+        trees.append(defineTrait(name, parent, defineImplicitConv(empty, ev.name, ev.variable)))
+        Some(name)
+      }
+
+      val moduleName = s"${bound.evs.head.name.tpt}Conv"
+      val module = defineObject(moduleName, lastParent, defineImplicitConv(empty, bound.evs.head.name, bound.evs.head.variable))
+      trees.append(module)
+
+      val imp = importModule(moduleName)
+      trees.append(imp)
+
+      trees.append(nullVal(bound.typ.decode, empty))
+
+      trees.toList
+    }
 
     def injectTransformations(tree: Tree, bounds: List[ContextBound]): Tree = {
       val res = tree match {
         case d @ DefDef(mods, name, tparams, vparamss, tpt, rhs) =>
           rhs match {
-            case b @ Block(stats, expr) =>
-              println(s"Inserting F into $d")
-              val res = d.copy(rhs = b.copy(stats = valDef("F") :: stats))
-              println(s"Result looks like: $res")
+            case b: Block =>
+              val insert = bounds.flatMap(createTraits)
+              val res = d.copy(rhs = b.copy(stats = insert ::: b.stats))
+              println(s"Modified: $res")
               res
-            case _ =>
-              println("rhs is not a block")
-              tree
+            case value =>
+              val insert = bounds.flatMap(createTraits)
+              val res = d.copy(rhs = Block(insert, value))
+              println(s"Modified literal rhs: $res")
+              res
           }
         case _ =>
           println("Not DefDef")
@@ -78,7 +173,7 @@ class ContextPlugin(plugin: Plugin, val global: Global)
         val evs = vparamss.lastOption.toList.flatMap { params => params.collect { case Evidence(e) => e } }
         val bounds = tpars.flatMap { s =>
           val imps = evs.filter(ev => ev.typ == s)
-          if (imps.isEmpty) Nil
+          if (imps.isEmpty) List()
           else List(ContextBound(s, imps))
         }
         if (bounds.isEmpty) None
@@ -92,12 +187,13 @@ class ContextPlugin(plugin: Plugin, val global: Global)
 
   object Evidence {
     def unapply(valDef: ValDef): Option[Evidence] = valDef match {
-      case ValDef(mods, TermName(variable), AppliedTypeTree(Ident(instanceType), List(Ident(typ @ TypeName(_)))), _)
-        if mods.isImplicit => Some(Evidence(instanceType.decoded, typ, variable))
+      case ValDef(mods, TermName(variable), ap @ AppliedTypeTree(Ident(instanceType), List(Ident(typ @ TypeName(_)))), _)
+        if mods.isImplicit => Some(Evidence(ap, typ, variable))
       case _ => None
     }
   }
 
-  case class Evidence(name: String, typ: TypeName, variable: String)
+  //Evidence(Traverse[A],A,evidence$5)
+  case class Evidence(name: AppliedTypeTree, typ: TypeName, variable: String)
 
 }
